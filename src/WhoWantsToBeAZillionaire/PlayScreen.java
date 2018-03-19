29ae983644c2bfde3d7cc269ea00b2c0ebfcdb5b
u1759657 <U1759657@unimail.hud.ac.uk>
2018-03-19 17:49:03 +0000

    assignment edit 7

--- src/WhoWantsToBeAZillionaire/PlayScreen.java
+++ src/WhoWantsToBeAZillionaire/PlayScreen.java
@@ -1,43 +1,21 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
 
-public class PlayScreen extends JFrame implements ActionListener {
-    JButton questionButton, answer1Button, answer2Button, answer3Button, answer4Button, fiftyfiftyButton, asktheaudienceButton, equalizerButton, takethemoneyButton, submitanswerButton;
-    JTextPane categoryTextPane, questionAmountTextPane;
-    JSlider DifficultySlider;
-    JPanel panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8;
-
-    PlayScreen () {
-        Container cp = getContentPane();
-        cp.setLayout(new BorderLayout (3,4));
-
-        submitanswerButton = new JButton("Submit Answer");
-        takethemoneyButton = new JButton("Take The Money");
-        fiftyfiftyButton = new JButton("50/50");
-        asktheaudienceButton = new JButton("Ask The Audience");
-        equalizerButton = new JButton("Equalizer");
-
-        submitanswerButton.addActionListener(this);
-        takethemoneyButton.addActionListener(this);
-        fiftyfiftyButton.addActionListener(this);
-        asktheaudienceButton.addActionListener(this);
-        equalizerButton.addActionListener(this);
-
-        panel1 = new JPanel();
-        panel2 = new JPanel();
-        panel3 = new JPanel();
-        panel4 = new JPanel();
-        panel5 = new JPanel();
-        panel6 = new JPanel();
-        panel7 = new JPanel();
-        panel8 = new JPanel();
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-    }
-
-    public void actionPerformed(ActionEvent event) {
-
-    }
+public class PlayScreen {
+    private JTextPane categoryDisplayTextPane;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JSlider slider1;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JTextPane questionAmountDisplayTextPane;
+    private JButton button9;
+    private JButton button10;
 }
