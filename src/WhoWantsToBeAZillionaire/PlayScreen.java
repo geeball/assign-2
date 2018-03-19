e467e27e7c713158b9fdb8487b49645300b72052
u1759657 <U1759657@unimail.hud.ac.uk>
2018-03-19 11:50:54 +0000

    assignment edit 6

--- src/WhoWantsToBeAZillionaire/PlayScreen.java
+++ src/WhoWantsToBeAZillionaire/PlayScreen.java
@@ -12,6 +12,29 @@ public class PlayScreen extends JFrame implements ActionListener {
         Container cp = getContentPane();
         cp.setLayout(new BorderLayout (3,4));
 
+        submitanswerButton = new JButton("Submit Answer");
+        takethemoneyButton = new JButton("Take The Money");
+        fiftyfiftyButton = new JButton("50/50");
+        asktheaudienceButton = new JButton("Ask The Audience");
+        equalizerButton = new JButton("Equalizer");
+
+        submitanswerButton.addActionListener(this);
+        takethemoneyButton.addActionListener(this);
+        fiftyfiftyButton.addActionListener(this);
+        asktheaudienceButton.addActionListener(this);
+        equalizerButton.addActionListener(this);
+
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+        panel3 = new JPanel();
+        panel4 = new JPanel();
+        panel5 = new JPanel();
+        panel6 = new JPanel();
+        panel7 = new JPanel();
+        panel8 = new JPanel();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
 
     public void actionPerformed(ActionEvent event) {
