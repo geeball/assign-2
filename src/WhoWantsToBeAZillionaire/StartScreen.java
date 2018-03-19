b0b87063ae6a3bdb418e9906d1c8a03709e4367d
u1759657 <U1759657@unimail.hud.ac.uk>
2018-03-19 17:50:06 +0000

    assignment edit 8

--- src/WhoWantsToBeAZillionaire/StartScreen.java
+++ src/WhoWantsToBeAZillionaire/StartScreen.java
@@ -4,4 +4,6 @@ public class StartScreen {
     private JTextPane whoWantsToBeTextPane;
     private JButton button1;
     private JButton button2;
+    private JSlider slider1;
+    private JTextPane difficultySliderTextPane;
 }
