a1e42946d2ac07f5da2f71648238f68d377da925
u1759657 <U1759657@unimail.hud.ac.uk>
2018-03-21 19:39:08 +0000

    assignment edit 11

--- src/WhoWantsToBeAZillionaire/Player.java
+++ src/WhoWantsToBeAZillionaire/Player.java
@@ -1,8 +1,6 @@
-package WhoWantsToBeAZillionaire;
-
 /**
  * Created by U1759657 on 26/02/2018.
  */
 public class Player {
-
+    private String playerName;
 }
---
+++ src/WhoWantsToBeAZillionaire/WhoWantsToBeAZillionaire/Zillionaire.java
@@ -0,0 +1,4 @@
+package WhoWantsToBeAZillionaire;
+
+public class Zillionaire {
+}
