74df610bfc3cf534b84edd64a04a5968d9bb559e
u1759657 <U1759657@unimail.hud.ac.uk>
2018-03-22 10:25:47 +0000

    assignment edit 14

--- src/WhoWantsToBeAZillionaire/Player.java
+++ src/WhoWantsToBeAZillionaire/Player.java
@@ -4,12 +4,24 @@
 public class Player {
     private String playerName;
 
+    public Player(String playerName) {
+        // this is me making sure the afore declared variables can be accessed by other classes
+        this.playerName = playerName;
+    }
+
     public String getPlayerName() {
         return playerName;
     }
 
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerName='" + playerName + '\'' +
+                '}';
     }
 
     public static void main(String[] args) {
--- src/WhoWantsToBeAZillionaire/Players.java
+++ src/WhoWantsToBeAZillionaire/Players.java
@@ -2,4 +2,22 @@ import java.util.ArrayList;
 
 public class Players {
     private ArrayList<Player> zillionaiirePlayers = new ArrayList<>();
+
+    public Players() {
+        Player one = new Player("John");
+        Player two = new Player("Mary");
+        zillionaiirePlayers.add(one);
+        zillionaiirePlayers.add(two);
+
+        printPlayers();
+    }
+
+    public void printPlayers(){
+        for (Player player : zillionaiirePlayers) {
+            System.out.println(player);
+        }
+    }
+    public static void main(String[] args) {
+        new Players();
+    }
 }
